4140154409d592351eb19fc6cc2643d2113148a9
Duncalicious <dbru997@gmail.com>
2021-02-22 11:53:37 -0700

    Program creates balanced binary search trees and prints them nicely

---
+++ balanced_bst.rb
@@ -0,0 +1,46 @@
+sorted_array = [1,2,3,4,5,6,7,8]
+
+class Node
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(value)
+    @data = value
+    @left = nil
+    @right = nil
+  end
+end
+
+class Tree
+  attr_reader :tree
+  attr_accessor :root
+
+  def initialize(data)
+    @root = build_tree(data)
+  end
+
+  def build_tree(data)
+    return nil if 0 > data.length-1
+    if data.length == 1
+      return Node.new(data[0])
+    end
+    mid = data.length/2
+
+    root = Node.new(data[mid])
+    data[0..mid-1]
+    root.left = build_tree(data[0..mid-1])
+    root.right = build_tree(data[mid+1..-1])
+    return root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
+
+a = Tree.new(sorted_array)
+a.pretty_print
+
+# spacer
